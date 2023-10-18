b09b32cd362eb684c2fad2356578d13c0a842459
Kelvin <kelzlaur34@gmail.com>
2023-10-17 21:35:16 -0400

    updated react

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,10 +1,21 @@
-
 import './App.css';
+import AddExpenseForm from './pages/AddExpenseForm';
+import EditBudget from './pages/EditBudget';
+import ExpenseItem from './pages/ExpenseItem';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 function App() {
+
+
   return (
     <div className="App">
-      
+      <NavBar />
+
+      <AddExpenseForm />
+      <EditBudget />
+      <ExpenseItem />
+      <Home />
     </div>
   );
 }
---
+++ budget/src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = (props) => {
+    return (
+    <nav className="Navbar">
+      <div className="links">
+        <a href="/create">Transaction History</a>
+        <a href="/">About</a>
+      </div>
+    </nav>
+    );
+  }
+  
+  export default NavBar;
