f46bab3587ebe4e723d2142b338d17ecb2488733
juliaeu <juliabenedic13@gmail.com>
2023-10-24 23:52:50 +0100

    UserLoginForm & LoginFrom

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Budget from './components/Budget'
+import Budget from './components/Budget';
 import AddExpenseForm from './components/AddExpenseForm';
-import EditBudget from './components/EditBudget';
 import ExpenseItem from './components/ExpenseItem';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
+import UserLoginForm from './components/UserLoginForm'; // Update this line
 import { AppProvider } from './context/AppContext';
 
 const App = () => {
   return (
     <AppProvider>
       <div className='container'>
-      <h1 className='mt-3'>Expense App</h1>
+        <h1 className='mt-3'>Expense App</h1>
         <div className="row mt-3">
           <div className="col-sm">
             <Budget />
-        </div>
-        <div className='col-sm'>
-            <Remaining  />
-        </div>
-        <div className='col-sm'>
+          </div>
+          <div className='col-sm'>
+            <Remaining />
+          </div>
+          <div className='col-sm'>
             <ExpenseTotal />
+          </div>
         </div>
-      </div>
-      <h3 className='mt-3'>Expenses</h3>
-      <div className='row mt-3'>
+        <h3 className='mt-3'>Expenses</h3>
+        <div className='row mt-3'>
           <div className='col-sm'>
             <ExpenseList />
           </div>
-      </div>
-      <h3 className='mt-3'>App Expense</h3>
-      <div className='mt-3'>
-        <div className='col-sm'>
-          <AddExpenseForm />
         </div>
+        <h3 className='mt-3'>App Expense</h3>
+        <div className='mt-3'>
+          <div className='col-sm'>
+            <AddExpenseForm />
+          </div>
+        </div>
+        <UserLoginForm /> {/* Add this line to render the UserLoginForm component */}
       </div>
-    </div>
     </AppProvider>
   );
 };
 
 export default App;
+
+
